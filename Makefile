170d52f3af99fd6ce3f337b155671df3fb7684fe
Abhishiv Saxena <abhishiv@gmail.com>
2020-11-29 11:26:15 +0100

    webpack config

--- Makefile
+++ Makefile
@@ -28,7 +28,7 @@ doc:
 	./node_modules/.bin/docco -o . -x md -l plain  ./src/index.ts
 	rm -rf ./Readme.md
 	mv ./src/index.md ./Readme.md
-	./node_modules/.bin/docco -o .  -l parallel  ./src/index.ts
+	./node_modules/.bin/docco -o .  -l ../../tools/docco/theme/  ./src/index.ts
 	mv ./src/index.html ./index.html
 	./node_modules/.bin/typedoc --theme ../../tools/typedoc/default  --out ./docs/types   --includeDeclarations --exclude "**/node_modules/**/*" --inputFiles ./src
 
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@
 // To use run `npm install -g @gratico/atom`
 //
 
-import { applyPatch, Operation, BaseOperation } from "fast-json-patch";
+import { applyPatch } from "fast-json-patch";
 
 export interface IPatch {
   path: string[];
@@ -22,9 +22,10 @@ export type ICommit = IPatch[];
 export interface IAtom<T = any> {
   state: T;
   handlers: Map<string, Function>;
+  watchPaths: Map<string, string[]>;
 }
 
-const get = (state: any, path: string[]) =>
+export const get = (state: any, path: string[]) =>
   path.reduce((res, prop) => res[prop], state);
 
 export function deref(store: IAtom, path: string[]) {
@@ -35,19 +36,22 @@ export function defAtom<T = any>(state: T) {
   const atom = new Atom(state);
   return atom;
 }
-export function defCursorUnsafe(store: IAtom, path: string[]) {
+
+export function defCursor(store: IAtom, path: string[]) {
   return path;
 }
-export function defUpdatableCursor(store: IAtom, path: string[]) {
+export function defWatchableCursor(store: IAtom, path: string[]) {
   return {
     addWatch: function (
       wid: string,
       handler: (id: string, commit: ICommit) => void
     ) {
       store.handlers.set(wid, handler);
+      store.watchPaths.set(wid, path);
     },
     removeWatch: (wid: string) => {
       store.handlers.delete(wid);
+      store.watchPaths.delete(wid);
     },
   };
 }
@@ -66,8 +70,10 @@ export function commitPatch(store: IAtom, patch: ICommit) {
 export class Atom<T> implements IAtom<T> {
   state: T;
   handlers: Map<string, Function>;
+  watchPaths: Map<string, string[]>;
   constructor(state: T) {
     this.state = state;
     this.handlers = new Map();
+    this.watchPaths = new Map();
   }
 }
